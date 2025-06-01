64e669f38e6ba917f13519f4cc4a4294de340821
dahenkakyd <dahenkakyd@gmail.com>
2025-06-01 13:22:20 +0300

    lesson_3

--- 03_lesson/address.py
+++
@@ -1,9 +0,0 @@
-class Address:
-    def __init__(self, index, sity, stret, hous, apt ):
-        self.index = index
-        self.sity = sity
-        self.stret = stret
-        self.hous = hous    
-        self.apt = apt
-    def __str__(self):
-        return f"{self.index} {self.sity}, {self.stret}, {self.hous}, {self.apt}"
--- 03_lesson/lesson_3_task_1.py
+++
@@ -1,7 +0,0 @@
-from user import User
-my_user=User("Антонова","Дарья")
-
-
-my_user.sayName()
-my_user.saysurname()
-my_user.sayname_surname()
