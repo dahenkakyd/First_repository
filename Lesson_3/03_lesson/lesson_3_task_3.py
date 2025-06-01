c15031e06dffc0d3c284b1f83cadc2728677e487
dahenkakyd <dahenkakyd@gmail.com>
2025-06-01 13:59:25 +0300

    lesson_3

--- Lesson_3/03_lesson/lesson_3_task_3.py
+++ Lesson_3/03_lesson/lesson_3_task_3.py
@@ -2,9 +2,7 @@ from address import Address
 from mailing import Mailing
 
 to_address = Address(428003, "Чебоксары", "Президентский б-р", 14, 10)
-form_address = Address(428028, "Чебоксары", "пр-кт Тракторостроителей", 66, 32)
-form_address = Address(428027, "Чебоксары", "Кукшумская", 12, 10)
+from_address = Address(428028, "Чебоксары", "пр-кт Тракторостроителей", 66, 32)
 
 mailing = Mailing(to_address, from_address, cost=500, track="TR123")
 print(mailing)
-return f"Отправление {self.track} из {self.from_address} в {self.to_address}. Стоимость {self.cost} рублей."
